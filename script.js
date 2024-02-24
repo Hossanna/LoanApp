430e62f555ba1f1f20f5894574180e03c66347db
Hosanna Bivan <hosabivan@gmail.com>
2024-02-24 13:13:18 +0100

    remaining the date

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 // alert("connected")
 
-// new Date ("2024-01-19")
-// console.log(collection);
-
 function validateForm() {
   let loan = document.getElementById("loan").value;
   let revenue = document.getElementById("revenue").value;
@@ -21,13 +18,6 @@ function validateForm() {
   let accountType = document.getElementById("accountType").value;
   let points = 0;
 
-  // form.style.display = "none"
-  // myForm2.style.display = "block"
-  // username.innerText = `${name}`
-  // balance.innerText = `#${revenue}`
-  // loanLimit.innerText = `#${maxLoanAmount}`
-  // loanAmount.innerText = `#${loan}`
-
   // console.log(loan);
   // console.log(revenue);
   // console.log(name);
@@ -36,7 +26,7 @@ function validateForm() {
   // console.log(history);
   // console.log(deposit);
   // console.log(repayment);
-  console.log(points);
+  // console.log(points);
 
   //steps to calculate points
 
@@ -48,19 +38,52 @@ function validateForm() {
   } else {
     points = 0;
   }
-  console.log(points);
+  // console.log(points);
 
   // 6 months credit history, 10pts
   if (history >= 6) {
     points = points + 10;
   }
-  console.log(points);
+  // console.log(points);
 
   // loan repayment, below 6 months, 5 pts
   if (repayment < 6) {
     points = points + 5;
   }
-  console.log(points);
+  // console.log(points);
+
+  // current, 10pts, savings, 5pts
+  if (accountType == "savings") {
+    points = points + 5;
+  } else if (accountType == "current") {
+    points = points + 10;
+  }
+
+  // console.log(points);
+
+  // if points >= 30, loan is successful, else it is not
+  if (points >= 30) {
+    alert("Your Loan has been Approved");
+  } else {
+    alert(
+      "You do not have sufficient points for this loan, you have " +
+        points +
+        "points"
+    );
+  }
+  // console.log(points);
+
+  form.style.display = "none"
+  myForm2.style.display = "block"
+  username.innerText = `${name}`
+  balance.innerText = `#${revenue}`
+  loanLimit.innerText = `#${maxLoanAmount}`
+  loanAmount.innerText = `#${loan}`
+
+  return true;
+
+}
+
 
   // last loan collection date, above 6 months , 10pts
   // function subtract6Months(today) {
@@ -97,27 +120,3 @@ function validateForm() {
   //   points = points + 5;
   // }
   // console.log(points);
-
-  // current, 10pts, savings, 5pts
-  if (accountType == "savings") {
-    points = points + 5;
-  } else if (accountType == "current") {
-    points = points + 10;
-  }
-
-  console.log(points);
-
-  // if points >= 30, loan is successful, else it is not
-  if (points >= 30) {
-    alert("Your Loan has been Approved");
-  } else {
-    alert(
-      "You do not have sufficient points for this loan, you have " +
-        points +
-        "points"
-    );
-  }
-  console.log(points);
-
-  return true;
-}
