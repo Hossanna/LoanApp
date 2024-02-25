df62dc8c743b90770003d441f4c0db18c1a82d94
Hosanna Bivan <hosabivan@gmail.com>
2024-02-25 22:52:33 +0100

    Finally finally, can present

--- script.js
+++ script.js
@@ -16,8 +16,6 @@ function validateForm() {
   let loanLimit = document.getElementById("loanLimit");
   let loanAmount = document.getElementById("loanAmount");
   let loanAmountt = document.getElementById("loanAmountt");
-  let deposit = document.getElementById("deposit").value;
-  let collection = document.getElementById("collection").value;
   let accountType = document.getElementById("accountType").value;
   let repaymentDate = document.getElementById("repaymentDate");
   let repaymentDatee = document.getElementById("repaymentDatee");
@@ -32,34 +30,34 @@ function validateForm() {
   } else if (revenue < loan) {
     points = points - 10;
   }
-  console.log(points);
+  
 
   // 6 months credit history, 10pts
   if (history >= 6) {
     points = points + 10;
   }
-  console.log(points);
+  
 
 
   // last deposit date, within a month, 5pts
   if (doSomethingg() == true){
     points = points+5
   }
-  console.log(points);
+  
 
 
   //last loan collection date, above 6 months, 10pts
   if (doSomething() == true){
     points = points+10
   }
-  console.log(points);
+  
 
 
   // loan repayment, below 6 months, 5 pts
   if (repayment < 6 && repayment > 0) {
     points = points + 5;
   }
-  console.log(points);
+  
 
 
   // current, 10pts, savings, 5pts
@@ -68,55 +66,53 @@ function validateForm() {
   } else if (accountType == "current") {
     points = points + 10;
   }
-  console.log(points);
-
-
   
 
-  // // if points >= 30, loan is successful, else it is not
-  // if (points >= 30) {
-  //   alert("Your Loan has been Approved, you have " + points + "points");
-  //   form.style.display = "none";
-  //   myForm3.style.display = "block";
-  //   usernamee.innerText = `${name}`;
-  //   loanAmountt.innerText = `#${loan}`;
 
-  // } else {
-  //   alert(
-  //     "You do not have sufficient points for this loan, you have " +
-  //       points +
-  //       "points"
-  //   );
-  //   form.style.display = "none";
-  //   myForm2.style.display = "block";
-  //   username.innerText = `${name}`;
-  //   balance.innerText = `#${revenue}`;
-  //   loanLimit.innerText = `#${maxLoanAmount}`;
-  //   loanAmount.innerText = `#${loan}`;
-  // }
+  // if points >= 30, loan is successful, else it is not
+  if (points >= 30) {
+    alert("Your Loan has been Approved, you have " + points + "points");
+    form.style.display = "none";
+    myForm3.style.display = "block";
+    usernamee.innerText = `${name}`;
+    loanAmountt.innerText = `#${loan}`;
 
-  // let today = new Date();
+  } else {
+    alert(
+      "You do not have sufficient points for this loan, you have " +
+        points +
+        "points"
+    );
+    form.style.display = "none";
+    myForm2.style.display = "block";
+    username.innerText = `${name}`;
+    balance.innerText = `#${revenue}`;
+    loanLimit.innerText = `#${maxLoanAmount}`;
+    loanAmount.innerText = `#${loan}`;
+  }
 
-  // function addOneMonth(today) {
-  //   today.setMonth(today.getMonth() + 1);
+  let today = new Date();
 
-  //   const f = new Intl.DateTimeFormat("en-us", {
-  //     dateStyle: "full",
-  //   });
-  //   return f.format(today);
-  // }
+  function addOneMonth(today) {
+    today.setMonth(today.getMonth() + 1);
+
+    const f = new Intl.DateTimeFormat("en-us", {
+      dateStyle: "full",
+    });
+    return f.format(today);
+  }
   
-  // repaymentDate.innerText = addOneMonth(today);
+  repaymentDate.innerText = addOneMonth(today);
   
-  // repaymentDatee.innerText = addOneMonth(today);
+  repaymentDatee.innerText = addOneMonth(today);
   
-
-  console.log(points);
 
   return true;
 
   
 }
+
+
 
 let today = new Date();
   // console.log(today);
